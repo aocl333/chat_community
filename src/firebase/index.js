c228b9865ce0a008297a392d6eb49fc86aeae630
DESKTOP-0R2RH6S\2210C <aocl333@nate.com>
2023-02-13 13:27:46 +0900

    'slack-upload'

---
+++ src/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-x09R-5oJ9UPJBtSV3ZE1__ilPBa04nM",
+  authDomain: "slack-b23d1.firebaseapp.com",
+  databaseURL: "https://slack-b23d1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "slack-b23d1",
+  storageBucket: "slack-b23d1.appspot.com",
+  messagingSenderId: "923248228818",
+  appId: "1:923248228818:web:6fd4b7c3b63e106c68b00b"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
